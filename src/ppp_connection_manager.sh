2b7de7c89421431fe6db033a8e01245551194672
Noom <asupawat@users.noreply.github.com>
2021-06-04 14:08:44 +0700

    Update ppp_connection_manager.sh

--- src/ppp_connection_manager.sh
+++ src/ppp_connection_manager.sh
@@ -15,6 +15,5 @@ if [[ $MODEM_CONFIG -eq 0 ]]; then
     bash ppp_reconnect.sh
 else
     debug "Modem configuration is failed multiple times!" 
-    debug "Checkout other troubleshooting steps on docs.sixfab.com."
     exit 1
 fi
